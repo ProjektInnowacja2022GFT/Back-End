03b95390815f7e14dc1dfa63c13c5e5d02e6086d
Damian <you@example.com>
2022-05-26 15:08:43 +0200

    Delete unnecessary files

--- src/main/java/com/gft/gdesk/dto/UserModel.java
+++ src/main/java/com/gft/gdesk/dto/UserModel.java
@@ -8,13 +8,13 @@ import lombok.*;
 @NoArgsConstructor
 @Builder
 
-public class Users {
+public class UserModel {
     private long id;
     private String name;
     private String surname;
     private String company;
     private String email;
     private String password;
-    private String role = "USER";
-    private String status = "WAIT_FOR_APPROVAL";
+    private String STATUS = "WAIT_FOR_APPROVAL";
+    private static final String ROLE = "USER";
 }
