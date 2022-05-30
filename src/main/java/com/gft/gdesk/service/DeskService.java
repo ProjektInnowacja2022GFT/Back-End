1250aad4586b4c08f3d00a6a5fc4075361758eb9
Damian <pk-14@sr.dmcs.pl>
2022-05-30 20:02:10 +0200

    GDE-022 Remove initializing methods and unnecessary imports

--- src/main/java/com/gft/gdesk/service/DeskService.java
+++ src/main/java/com/gft/gdesk/service/DeskService.java
@@ -3,10 +3,7 @@ package com.gft.gdesk.service;
 
 import com.gft.gdesk.dto.Desk;
 import org.springframework.stereotype.Service;
-
-import javax.annotation.PostConstruct;
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 
 @Service
@@ -25,28 +22,4 @@ public class DeskService {
         return desks.get(id);
     }
 
-
-    @PostConstruct
-    public void setInitialUsers() {
-        this.desks.addAll(Arrays.asList(
-                Desk.builder().
-                        id(0L).
-                        sector("A").
-                        deskNumber(11).
-                        floor(1).
-                        build(),
-                Desk.builder().
-                        id(1L).
-                        sector("A").
-                        deskNumber(21).
-                        floor(1).
-                        build(),
-                Desk.builder().
-                        id(2L).
-                        sector("C").
-                        deskNumber(37).
-                        floor(2).
-                        build()
-        ));
-    }
 }
